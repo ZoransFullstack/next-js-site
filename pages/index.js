1f6272f4f5f1b4a5efcb707059d05d77adb71b54
Zoran Bajic <zoranbajic@pm.me>
2021-12-10 14:28:29 -0600

    style: add styling to components

--- pages/index.js
+++ pages/index.js
@@ -5,23 +5,25 @@ import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <h1>Homepage</h1>
-      <p>
+    <div>
+      <h1 className={styles.title}>Homepage</h1>
+      <p className={styles.text}>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
         veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
         commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
         velit esse cillum dolore eu fugiat nulla pariatur.
       </p>
-      <p>
+      <p className={styles.text}>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
         veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
         commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
         velit esse cillum dolore eu fugiat nulla pariatur.
       </p>
-      <Link href='/coders'>See Coder Listing</Link>
+      <Link href='/coders'>
+        <a className={styles.btn}>See Coder Listing</a>
+      </Link>
     </div>
   );
 }
