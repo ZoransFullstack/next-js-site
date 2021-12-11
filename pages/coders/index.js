bc70b79f9fe6641050e3c51d312113a008043996
Zoran Bajic <zoranbajic@pm.me>
2021-12-10 22:44:08 -0600

    chore: connect to user api

--- pages/coders/index.js
+++ pages/coders/index.js
@@ -1,7 +1,27 @@
-const Coders = () => {
+import styles from '../../styles/Coders.module.css';
+
+// This runs at build time when our components are rendered
+// It does not run in the browser
+export const getStaticProps = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
+
+  return {
+    props: { coders: data },
+  };
+};
+
+const Coders = ({ coders }) => {
   return (
     <div>
       <h1>All Coders</h1>
+      {coders.map((coder) => (
+        <div key={coder.id}>
+          <a className={styles.single}>
+            <h3>{coder.name}</h3>
+          </a>
+        </div>
+      ))}
     </div>
   );
 };
